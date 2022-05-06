533bf209b1f4bc1892ebd67207d826ab9e3c100f
HacenWey <elhassenidoumo@gmail.com>
2022-05-06 16:57:07 +0000

    fix bug

--- src/CodeGeneratorServiceProvider.php
+++ src/CodeGeneratorServiceProvider.php
@@ -20,9 +20,7 @@ class CodeGeneratorServiceProvider extends ServiceProvider
             __DIR__ . '/resources/stubs' => resource_path('vendor/hacen-miske/stubs'),
 
         ]);
-        $this->publishes([
-            __DIR__ . '/config/crudSettings.json' => config_path('crudSettings.json'),
-        ]);
+        $this->mergeConfigFrom(__DIR__ . '/../config/crudSettings.json', 'crudSettings');
     }
 
 
