5ec7dc4ab3ced51689a09de4db9eab8e75e03331
HacenWey <elhassenidoumo@gmail.com>
2022-05-09 12:45:11 +0100

    fix-bug

--- src/CodeGeneratorServices/CrudGeneratorService.php
+++ src/CodeGeneratorServices/CrudGeneratorService.php
@@ -115,7 +115,7 @@ class CrudGeneratorService
     {
         $path_to_file  = base_path('routes/api.php');
         $append_route = 'Route::apiResource(\'' . Str::plural(strtolower($name)) . "', {$name}Controller::class); \n";
-        $append_import = 'use App\Http\Controllers\\' . $name . "Controller; \n";
+        $append_import = 'use App\Http\Controllers\\' . $name . "Controller;";
         File::append($path_to_file, $append_route);
         $file = str_replace(['<?php'], ["<?php \n " . $append_import], file_get_contents($path_to_file));
         file_put_contents($path_to_file, $file);
